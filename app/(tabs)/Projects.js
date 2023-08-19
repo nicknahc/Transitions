ff895fd050ffd7cc02abb7d5de0d5c9ef25bbe4c
Nicholas Chan <99701277+nicknahc@users.noreply.github.com>
2023-08-18 18:05:58 -0700

    edit project name works

--- app/(tabs)/Projects.js
+++ app/(tabs)/Projects.js
@@ -1,14 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { View, ScrollView, StyleSheet, PanResponder, TouchableOpacity, Text } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, ScrollView, StyleSheet, PanResponder, TouchableOpacity, Text, TextInput } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation, Link, useRouter} from "expo-router";
+import { useNavigation, Link, useRouter } from "expo-router";
 import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Assuming you want to use FontAwesome icons
 
 const Projects = () => {
   const navigation = useNavigation();
   const router = useRouter();
   const [projects, setProjects] = useState([]);
-
+   
+  const getProjectPressHandler = (projectIndex) => {
+    return () => {
+      console.log(projectIndex);
+      console.log(JSON.stringify(projects));
+      router.push({ pathname: `/${projectIndex}` });
+    };
+  };
+  
   const loadProjectsFromAsyncStorage = async () => {
     try {
       const storedProjects = await AsyncStorage.getItem('projects');
@@ -19,15 +28,7 @@ const Projects = () => {
       console.error('Error loading projects:', error);
     }
   };
-
-  const getProjectPressHandler = (projectIndex) => {
-    return () => {
-      console.log(projectIndex);
-      console.log(JSON.stringify(projects));
-      router.push({ pathname: `/${projectIndex}` });
-    };
-  };
-
+  
   const saveProjectsToAsyncStorage = async (updatedProjects) => {
     try {
       await AsyncStorage.setItem('projects', JSON.stringify(updatedProjects));
@@ -40,6 +41,35 @@ const Projects = () => {
     loadProjectsFromAsyncStorage();
   }, []);
 
+  const finishEditingProjectName = (projectIndex) => {
+    const updatedProjects = [...projects];
+    updatedProjects[projectIndex].editing = false;
+    setProjects(updatedProjects);
+    saveProjectsToAsyncStorage(updatedProjects);
+  };
+
+  const handleProjectNameChange = (projectIndex, newName) => {
+    const updatedProjects = [...projects];
+    updatedProjects[projectIndex].name = newName;
+    setProjects(updatedProjects);
+  };
+
+  const cancelEditingProjectName = (projectIndex) => {
+    const updatedProjects = [...projects];
+    if (updatedProjects[projectIndex].originalName) {
+      updatedProjects[projectIndex].name = updatedProjects[projectIndex].originalName;
+    }
+    updatedProjects[projectIndex].editing = false;
+    setProjects(updatedProjects);
+  };
+
+  const editProjectName = (projectIndex) => {
+    const updatedProjects = [...projects];
+    updatedProjects[projectIndex].editing = true;
+    updatedProjects[projectIndex].originalName = updatedProjects[projectIndex].name;
+    setProjects(updatedProjects);
+  };
+
   const createProject = () => {
     const newProject = {
       name: `Project ${projects.length + 1}`,
@@ -50,7 +80,6 @@ const Projects = () => {
     saveProjectsToAsyncStorage([...projects, newProject]);
   };
 
-
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.createButton} onPress={createProject}>
@@ -58,14 +87,45 @@ const Projects = () => {
       </TouchableOpacity>
       <ScrollView style={styles.projectsContainer}>
         {projects.map((project, projectIndex) => (
-          <TouchableOpacity
-            key={projectIndex}
-            onPress={getProjectPressHandler(project.index)}
-            style={[
-              styles.projectButton
-            ]}>
-            <Text>{project.name}</Text>
-          </TouchableOpacity>
+          <View key={projectIndex} style={styles.projectItem}>
+            <TouchableOpacity
+              onPress={getProjectPressHandler(project.index)}
+              style={[styles.projectButton]}
+            >
+              {project.editing ? (
+                <View style={styles.editableProjectNameContainer}>
+                  <TextInput
+                    style={styles.editableProjectName}
+                    value={project.name}
+                    onChangeText={(newName) =>
+                      handleProjectNameChange(projectIndex, newName)
+                    }
+                    autoFocus
+                  />
+                  <View style={styles.editButtonsContainer}>
+                    <TouchableOpacity
+                      onPress={() => finishEditingProjectName(projectIndex)}
+                    >
+                      <Text style={styles.saveButton}>Save</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      onPress={() => cancelEditingProjectName(projectIndex)}
+                    >
+                      <Text style={styles.cancelButton}>Cancel</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              ) : (
+                <Text>{project.name}</Text>
+              )}
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => editProjectName(projectIndex)}
+              style={styles.editButton}
+            >
+              <Icon name="edit" size={20} color="gray" />
+            </TouchableOpacity>
+          </View>
         ))}
       </ScrollView>
     </View>
@@ -96,6 +156,21 @@ const styles = StyleSheet.create({
     padding: 10,
     margin: 10,
     borderRadius: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  projectItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  editableProjectName: {
+    flex: 1,
+    fontSize: 16,
+  },
+  editButton: {
+    paddingHorizontal: 10,
   },
   focusedProject: {
     backgroundColor: 'lightblue',
@@ -106,6 +181,25 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     backgroundColor: 'blue',
     position: 'absolute',
+  },
+  editableProjectNameContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  editableProjectName: {
+    flex: 1,
+    fontSize: 16,
+  },
+  editButtonsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  saveButton: {
+    color: 'green',
+    marginRight: 10,
+  },
+  cancelButton: {
+    color: 'red',
   },
 });
 
